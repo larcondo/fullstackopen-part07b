fa205e1238309f725a794000404e134cc12eb9ce
larcondo <larcondo90@gmail.com>
2023-09-04 16:12:53 -0300

    modify code to have 2 counters

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,15 @@ const useCounter = () => {
 }
 
 function App() {
-  const counter = useCounter()
+  const left = useCounter()
+  const right = useCounter()
 
   return (
     <div>
-      <div>{ counter.value }</div>
-      <button onClick={ counter.increase }>plus</button>
-      <button onClick={ counter.decrease }>minus</button>
-      <button onClick={ counter.zero }>zero</button>
+      { left.value }
+      <button onClick={ left.increase }>left</button>
+      <button onClick={ right.increase }>right</button>
+      { right.value }
     </div>
   )
 }
